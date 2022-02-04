8c8e3cfd70d40d43636b0739b09a3ad4d548c51e
Isabella Firmino <isabella.mfbr@gmail.com>
2022-02-04 16:40:05 -0400

    Ajustando requisição

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 function teste(url){
+    url = 'https://randomuser.me/api/'
     let request = new XMLHttpRequest();
     request.open('GET', url, false);
     request.send();
-    return request.responseText
+    return request.responseText;  
 }
 
-alert(teste('https://randomuser.me/api/'));
+let data = teste();
+let users = JSON.parse(data);
+
+console.log(users)
+
+console.log(users.results[0].name.first)
+console.log(users.results[0].name.last)
