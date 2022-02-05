1f12016b97d64e7cb33009436e005e8af37c23da
Isabella Firmino <isabella.mfbr@gmail.com>
2022-02-04 20:54:37 -0400

    Criando o header

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-function teste(url){
+//fazendo a requisição http para pegar os dados da API
+function info(url){
     url = 'https://randomuser.me/api/'
     let request = new XMLHttpRequest();
     request.open('GET', url, false);
@@ -6,10 +7,31 @@ function teste(url){
     return request.responseText;  
 }
 
-let data = teste();
+//definindo as variáveis e constantes
+const data = info();
 let users = JSON.parse(data).results[0];
+let firstName = users.name.first;
+let lastName = users.name.last;
+let fullName = `${firstName} ${lastName}`
+let headerName = document.getElementById('header-name');
+let userAvatar = document.getElementById('user-avatar');
+let welcomeUser = document.getElementById('header-welcome-name-user');
+let welcomeMessage = document.getElementById('welcome');
+
+//setando o nome do usuário no header
+headerName.innerHTML = firstName;
+
+//verificando se o usuário é do gênero feminino ou masculino para exibir o avatar e mensagem de boas-vindas correspondente 
+if(users.gender == 'male'){
+    userAvatar.setAttribute('src', './assets/Avatar_masculino.svg');
+    welcomeMessage.innerHTML = "Bem vindo de volta";
+}else if(users.gender == 'female'){
+    userAvatar.setAttribute('src', './assets/Avatar_feminino.svg');
+    welcomeMessage.innerHTML = "Bem vinda de volta";
+}
+
+//inserindo o nome completo do usuário na mensagem de boas-vindas
+welcomeUser.innerHTML = fullName;
+
 
 console.log(users)
-
-console.log(users.name.first)
-console.log(users.name.last)
